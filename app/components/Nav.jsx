19e4cff407464c2a806291c0a283c086d81b28d0
Frans van der Zijde <vanderzijde@ubib.eur.nl>
2017-08-02 11:27:30 +0200

    Added custom styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,35 +4,38 @@ var { Link, IndexLink } = require('react-router');
 var Nav = () => {
 
    return (
-      <div className="top-bar">
-         <div className="top-bar-left">
-            <ul className="menu">
-               <li className="menu-text">React Timer App</li>
-               <li>
-                  <IndexLink to="/"
-                     activeClassName="active"
-                     activeStyle={{fontWeight: 'bold'}}>
-                     Timer
-                  </IndexLink>
-               </li>
-               <li>
-                  <Link to="/"
-                     activeClassName="active"
-                     activeStyle={{fontWeight: 'bold'}}>
-                     CountDown
-                  </Link>
-               </li>
-            </ul>
-         </div>
-         <div className="top-bar-right">
-            <div className="menu">
-               Created by <a href="#" target="_blank">Frans van der Zijde</a>
+      <div>
+         <div className="top-bar">
+            <div className="top-bar-left">
+               <ul className="menu">
+                  <li className="menu-text">React Timer App</li>
+                  <li>
+                     <IndexLink to="/"
+                        activeClassName="active"
+                        activeStyle={{ fontWeight: 'bold'}}>
+                        Timer
+                     </IndexLink>
+                  </li>
+                  <li>
+                     <Link to="/"
+                        activeClassName="active"
+                        activeStyle={{ fontWeight: 'bold'}}>
+                        Countdown
+                     </Link>
+                  </li>
+               </ul>
+            </div>
+            <div className="top-bar-right">
+               <ul className="menu">
+                  <li className="menu-text">
+                  Created by <a href="https://github.com/fvdz/ReactTimerApp"
+                     target="_blank">Frans van der Zijde</a>
+                  </li>
+               </ul>
             </div>
          </div>
       </div>
-
    );
-
 };
 
 module.exports = Nav;
